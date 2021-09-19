26924b2aa925b2e435b06ffc3607aa0feda836bd
DragonOfDissent <dvpsherman@gmail.com>
2021-09-19 17:41:49 -0500

    Adding more comments

--- src/main.kt
+++ src/main.kt
@@ -1,11 +1,13 @@
 fun main(){
     // Initialize variables
+    // Result might be redundant, but I like the organization
     val a = 10
     val b = 2
+    var result = 0
 
     // Perform calculations and assign it to result
     // Print the operation and the result
-    var result = a+b
+    result = a+b
     println("$a + $b = $result")
 
     result = a/b
