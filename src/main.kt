2bb6a0ee3ca75e1724f9c29bf479b1cbfe763e3b
DragonOfDissent <dvpsherman@gmail.com>
2021-09-19 17:33:17 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val a = 10
+    val b = 2
+    var result = 0
+
+    result = a+b
+    println("$a + $b = $result")
+
+    result = a/b
+    println("$a / $b = $result")
+
+    result = a*b
+    println("$a * $b = $result")
+
+    result = a-b
+    println("$a - $b = $result")
+
+    result = a%b
+    println("$a % $b = $result")
+}
